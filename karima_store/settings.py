c39c746f42912c86357d3cc79cd066ebb32e2b53
Mohamed Abdel Monsef <mohamedabdelmonsef886@gmail.com>
2025-10-15 23:08:56 +0300

    Fix vercel config

--- karima_store/settings.py
+++ karima_store/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-as1#p%1%p2%&t76se0#pw92%dm_js(vs0_6a*2e$jj7l94jv!%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'store', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
